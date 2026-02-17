9c377787f3a266b70fa2ddf3b9244d63c89bc106
emmdaz <emmanuel.cruzd@alumno.buap.mx>
2026-02-17 15:33:45 -0600

    Código para la creación de una base de datos en forma general dependiendo de las partículas que se detecten en la simulación. Esta base de datos servirá para el cálculo de la masa invariante de las partículas que se indiquen en la función.

--- invaviant-mass-calculator.py
+++ invaviant-mass-calculator.py
@@ -30,7 +30,7 @@ def inv_m(file, p1, p2, charge = 0, flavor1 = 0, flavor2 = 0, df = False, graph 
             phi1 = tree["Photon/Photon.Phi"].array(library = "np")
             c1 = False
             
-        elif flavor1 != 0:
+        elif p1 == "jet_1":
             pt1 = tree["Jet/Jet.PT"].array(library = "np")
             eta1 = tree["Jet/Jet.Eta"].array(library = "np")
             phi1 = tree["Jet/Jet.Phi"].array(library = "np")
@@ -57,7 +57,7 @@ def inv_m(file, p1, p2, charge = 0, flavor1 = 0, flavor2 = 0, df = False, graph 
             phi2 = tree["Photon/Photon.Phi"].array(library = "np")
             c2 = False
             
-        elif flavor2 != 0:
+        elif p2 == "jet_2":
             pt2 = tree["Jet/Jet.PT"].array(library = "np")
             eta2 = tree["Jet/Jet.Eta"].array(library = "np")
             phi2 = tree["Jet/Jet.Phi"].array(library = "np")
@@ -65,31 +65,31 @@ def inv_m(file, p1, p2, charge = 0, flavor1 = 0, flavor2 = 0, df = False, graph 
             c2 = True
     else:
         if p1 == "muon":
-            pt = tree["Muon/Muon.PT"].array(library = "np")
-            eta = tree["Muon/Muon.Eta"].array(library = "np")
-            phi = tree["Muon/Muon.Phi"].array(library = "np")
-            charge = tree["Muon/Muon.Charge"].array(library = "np")
-            c = True
+            pt1 = tree["Muon/Muon.PT"].array(library = "np")
+            eta1 = tree["Muon/Muon.Eta"].array(library = "np")
+            phi1 = tree["Muon/Muon.Phi"].array(library = "np")
+            charge1 = tree["Muon/Muon.Charge"].array(library = "np")
+            c1 = True
             
         elif p1 == "electron":
-            pt = tree["Electron/Electron.PT"].array(library = "np")
-            eta = tree["Electron/Electron.Eta"].array(library = "np")
-            phi = tree["Electron/Electron.Phi"].array(library = "np")
-            charge = tree["Electron/Electron.Charge"].array(library = "np")
-            c = True
+            pt1 = tree["Electron/Electron.PT"].array(library = "np")
+            eta1 = tree["Electron/Electron.Eta"].array(library = "np")
+            phi1 = tree["Electron/Electron.Phi"].array(library = "np")
+            charge1 = tree["Electron/Electron.Charge"].array(library = "np")
+            c1 = True
             
         elif p1 == "photon":
-            pt = tree["Photon/Photon.PT"].array(library = "np")
-            eta = tree["Photon/Photon.Eta"].array(library = "np")
-            phi = tree["Photon/Photon.Phi"].array(library = "np")
-            c = False
+            pt1 = tree["Photon/Photon.PT"].array(library = "np")
+            eta1 = tree["Photon/Photon.Eta"].array(library = "np")
+            phi1 = tree["Photon/Photon.Phi"].array(library = "np")
+            c1 = False
             
-        elif flavor1 != 0:
-            pt = tree["Jet/Jet.PT"].array(library = "np")
-            eta = tree["Jet/Jet.Eta"].array(library = "np")
-            phi = tree["Jet/Jet.Phi"].array(library = "np")
-            charge = tree["Jet/Jet.Charge"].array(library = "np")
-            c = True
+        elif p1 == "jet":
+            pt1 = tree["Jet/Jet.PT"].array(library = "np")
+            eta1 = tree["Jet/Jet.Eta"].array(library = "np")
+            phi1 = tree["Jet/Jet.Phi"].array(library = "np")
+            charge1 = tree["Jet/Jet.Charge"].array(library = "np")
+            c1 = True
         
     # The jet flavor is computed using the the Simple Δ𝑅
     # highest-flavor match and it is asigned as follows:
@@ -126,17 +126,17 @@ def inv_m(file, p1, p2, charge = 0, flavor1 = 0, flavor2 = 0, df = False, graph 
                     index1 = np.argmax(pt1[i])
                     index2 = np.argmax(pt2[i])
                     
-                    if charge1[i,index1] + charge2[i,index2] == charge:
+                    if charge1[i][index1] + charge2[i][index2] == charge:
                         
-                        pt_1.append(pt1[i,index1])
-                        eta_1.append(eta1[i,index1])
-                        phi_1.append(phi1[i,index1])
-                        cha_1.append(charge1[i,index1])
+                        pt_1.append(pt1[i][index1])
+                        eta_1.append(eta1[i][index1])
+                        phi_1.append(phi1[i][index1])
+                        cha_1.append(charge1[i][index1])
                             
-                        pt_2.append(pt2[i,index2])
-                        eta_2.append(eta2[i,index2])
-                        phi_2.append(phi2[i,index2])
-                        cha_2.append(charge2[i,index2])
+                        pt_2.append(pt2[i][index2])
+                        eta_2.append(eta2[i][index2])
+                        phi_2.append(phi2[i][index2])
+                        cha_2.append(charge2[i][index2])
                             
                         event_1.append(i)
                         event_2.append(i)
@@ -167,16 +167,16 @@ def inv_m(file, p1, p2, charge = 0, flavor1 = 0, flavor2 = 0, df = False, graph 
                         index1 = np.argmax(pt1[i])
                         index2 = np.argmax(pt2[i])
 
-                        if charge1[i,index1] + charge2[i,index2] == charge:
-                            pt_1.append(pt1[i,index1])
-                            eta_1.append(eta1[i,index1])
-                            phi_1.append(phi1[i,index1])
-                            cha_1.append(charge1[i,index1])
+                        if charge1[i][index1] + charge2[i][index2] == charge:
+                            pt_1.append(pt1[i][index1])
+                            eta_1.append(eta1[i][index1])
+                            phi_1.append(phi1[i][index1])
+                            cha_1.append(charge1[i][index1])
                                 
-                            pt_2.append(pt2[i,index2])
-                            eta_2.append(eta2[i,index2])
-                            phi_2.append(phi2[i,index2])
-                            cha_2.append(charge2[i,index2])
+                            pt_2.append(pt2[i][index2])
+                            eta_2.append(eta2[i][index2])
+                            phi_2.append(phi2[i][index2])
+                            cha_2.append(charge2[i][index2])
                                 
                             event_1.append(i)
                             event_2.append(i)             
@@ -190,16 +190,16 @@ def inv_m(file, p1, p2, charge = 0, flavor1 = 0, flavor2 = 0, df = False, graph 
                         index1 = np.argmax(pt1[i])
                         index2 = np.argmax(pt2[i])
 
-                        if charge1[i,index1] + charge2[i,index2] == charge:
-                            pt_1.append(pt1[i,index1])
-                            eta_1.append(eta1[i,index1])
-                            phi_1.append(phi1[i,index1])
-                            cha_1.append(charge1[i,index1])
+                        if charge1[i][index1] + charge2[i][index2] == charge:
+                            pt_1.append(pt1[i][index1])
+                            eta_1.append(eta1[i][index1])
+                            phi_1.append(phi1[i][index1])
+                            cha_1.append(charge1[i][index1])
                                 
-                            pt_2.append(pt2[i,index2])
-                            eta_2.append(eta2[i,index2])
-                            phi_2.append(phi2[i,index2])
-                            cha_2.append(charge2[i,index2])
+                            pt_2.append(pt2[i][index2])
+                            eta_2.append(eta2[i][index2])
+                            phi_2.append(phi2[i][index2])
+                            cha_2.append(charge2[i][index2])
                                 
                             event_1.append(i)
                             event_2.append(i)
@@ -213,16 +213,16 @@ def inv_m(file, p1, p2, charge = 0, flavor1 = 0, flavor2 = 0, df = False, graph 
                         index1 = np.argmax(pt1[i])
                         index2 = np.argmax(pt2[i])
 
-                        if charge1[i,index1] + charge2[i,index2] == charge:
-                            pt_1.append(pt1[i,index1])
-                            eta_1.append(eta1[i,index1])
-                            phi_1.append(phi1[i,index1])
-                            cha_1.append(charge1[i,index1])
+                        if charge1[i][index1] + charge2[i][index2] == charge:
+                            pt_1.append(pt1[i][index1])
+                            eta_1.append(eta1[i][index1])
+                            phi_1.append(phi1[i][index1])
+                            cha_1.append(charge1[i][index1])
                                 
-                            pt_2.append(pt2[i,index2])
-                            eta_2.append(eta2[i,index2])
-                            phi_2.append(phi2[i,index2])
-                            cha_2.append(charge2[i,index2])
+                            pt_2.append(pt2[i][index2])
+                            eta_2.append(eta2[i][index2])
+                            phi_2.append(phi2[i][index2])
+                            cha_2.append(charge2[i][index2])
                                 
                             event_1.append(i)
                             event_2.append(i)
@@ -282,30 +282,30 @@ def inv_m(file, p1, p2, charge = 0, flavor1 = 0, flavor2 = 0, df = False, graph 
                     index1 = np.argmax(pt1[i])
                     index2 = np.argmax(pt2[i])
 
-                    if c1 == True and charge1[i,index1] == charge:
-                        charge_12.append(charge1[i,index1])
+                    if c1 == True and charge1[i][index1] == charge:
+                        charge_12.append(charge1[i][index1])
 
-                        pt_1.append(pt1[i,index1])
-                        eta_1.append(eta1[i,index1])
-                        phi_1.append(phi1[i,index1])
+                        pt_1.append(pt1[i][index1])
+                        eta_1.append(eta1[i][index1])
+                        phi_1.append(phi1[i][index1])
                             
-                        pt_2.append(pt2[i,index2])
-                        eta_2.append(eta2[i,index2])
-                        phi_2.append(phi2[i,index2])
+                        pt_2.append(pt2[i][index2])
+                        eta_2.append(eta2[i][index2])
+                        phi_2.append(phi2[i][index2])
                             
                         event_1.append(i)
                         event_2.append(i)
 
-                    elif charge2[i,index2] == charge:
-                        charge_12.append(charge2[i,index2])
+                    elif charge2[i][index2] == charge:
+                        charge_12.append(charge2[i][index2])
 
-                        pt_1.append(pt1[i,index1])
-                        eta_1.append(eta1[i,index1])
-                        phi_1.append(phi1[i,index1])
+                        pt_1.append(pt1[i][index1])
+                        eta_1.append(eta1[i][index1])
+                        phi_1.append(phi1[i][index1])
                             
-                        pt_2.append(pt2[i,index2])
-                        eta_2.append(eta2[i,index2])
-                        phi_2.append(phi2[i,index2])
+                        pt_2.append(pt2[i][index2])
+                        eta_2.append(eta2[i][index2])
+                        phi_2.append(phi2[i][index2])
                             
                         event_1.append(i)
                         event_2.append(i)
@@ -321,16 +321,16 @@ def inv_m(file, p1, p2, charge = 0, flavor1 = 0, flavor2 = 0, df = False, graph 
                         index1 = np.argmax(pt1[i])
                         index2 = np.argmax(pt2[i])
 
-                        if charge1[i,index1] == charge:
-                            charge_12.append(charge1[i,index1])
+                        if charge1[i][index1] == charge:
+                            charge_12.append(charge1[i][index1])
 
-                            pt_1.append(pt1[i,index1])
-                            eta_1.append(eta1[i,index1])
+                            pt_1.append(pt1[i][index1])
+                            eta_1.append(eta1[i][index1])
                             phi_1.append(phi1[i,index1])
                                 
-                            pt_2.append(pt2[i,index2])
-                            eta_2.append(eta2[i,index2])
-                            phi_2.append(phi2[i,index2])
+                            pt_2.append(pt2[i][index2])
+                            eta_2.append(eta2[i][index2])
+                            phi_2.append(phi2[i][index2])
                                 
                             event_1.append(i)
                             event_2.append(i)
@@ -343,16 +343,16 @@ def inv_m(file, p1, p2, charge = 0, flavor1 = 0, flavor2 = 0, df = False, graph 
                         index1 = np.argmax(pt1[i])
                         index2 = np.argmax(pt2[i])
 
-                        if charge2[i,index2] == charge:
-                            charge_12.append(charge2[i,index2])
+                        if charge2[i][index2] == charge:
+                            charge_12.append(charge2[i][index2])
 
-                            pt_1.append(pt1[i,index1])
-                            eta_1.append(eta1[i,index1])
-                            phi_1.append(phi1[i,index1])
+                            pt_1.append(pt1[i][index1])
+                            eta_1.append(eta1[i][index1])
+                            phi_1.append(phi1[i][index1])
                                 
-                            pt_2.append(pt2[i,index2])
-                            eta_2.append(eta2[i,index2])
-                            phi_2.append(phi2[i,index2])
+                            pt_2.append(pt2[i][index2])
+                            eta_2.append(eta2[i][index2])
+                            phi_2.append(phi2[i][index2])
                                 
                             event_1.append(i)
                             event_2.append(i)
@@ -382,6 +382,181 @@ def inv_m(file, p1, p2, charge = 0, flavor1 = 0, flavor2 = 0, df = False, graph 
                     }
                 
             data = pd.DataFrame(data)
+
+    # In the case of detections of particles from the same type.
+    # Events with no photons:
+    elif p1 == p2 and (c1 == True):
+        
+        events = len(pt1)
+        
+        pt_1 = []
+        eta_1 = []
+        phi_1 = []
+        cha_1 = []
+        
+        pt_2 = []
+        eta_2 = []
+        phi_2 = []
+        cha_2 = []
+        
+        event_1 = []
+        event_2 = []
+
+        # Particles that aren't jets:
+        if p1 != "jet":
+            
+            for i in range(events):
+                if len(pt1[i]) > 1: # At least two particles
+                    index1 = np.argmax(pt1[i])
+                    trial_pt1 = pt1[i][index1]
+
+                    pt1[i][index1] = 1e-1000
+
+                    index2 = np.argmax(pt1[i])
+                    trial_pt2 = pt1[i][index2]
+
+                    if charge1[i][index1] + charge1[i][index2] == charge:
+                        
+                        pt_1.append(trial_pt1)
+                        eta_1.append(eta1[i][index1])
+                        phi_1.append(phi1[i][index1])
+                        cha_1.append(charge1[i][index1])
+                            
+                        pt_2.append(trial_pt2)
+                        eta_2.append(eta2[i][index2])
+                        phi_2.append(phi2[i][index2])
+                        cha_2.append(charge2[i][index2])
+                            
+                        event_1.append(i)
+                        event_2.append(i)
+
+        # Events with jets:
+        elif p1 == "jet":
+            if flavor1 == flavor2 and flavor1 != 0:
+                for i in range(events):
+                    suma_1 = np.sum(flavor[i] == flavor1)
+
+                    if suma_1 > 1: # At least two particles
+                        index1 = np.argmax(pt1[i])
+                        trial_pt1 = pt1[i][index1]
+
+                        pt1[i][index1] = 1e-1000
+
+                        index2 = np.argmax(pt1[i])
+                        trial_pt2 = pt1[i][index2]
+
+                        if charge1[i][index1] + charge1[i][index2] == charge:
+                            
+                            pt_1.append(trial_pt1)
+                            eta_1.append(eta1[i][index1])
+                            phi_1.append(phi1[i][index1])
+                            cha_1.append(charge1[i][index1])
+                                
+                            pt_2.append(trial_pt2)
+                            eta_2.append(eta2[i][index2])
+                            phi_2.append(phi2[i][index2])
+                            cha_2.append(charge2[i][index2])
+                                
+                            event_1.append(i)
+                            event_2.append(i)
+
+        pt_1 = np.array(pt_1)
+        eta_1 = np.array(eta_1)
+        phi_1 = np.array(phi_1)
+        cha_1 = np.array(cha_1)
+            
+        pt_2 = np.array(pt_2)
+        eta_2 = np.array(eta_2)
+        phi_2 = np.array(phi_2)
+        cha_2 = np.array(cha_2)
+             
+        event_1 = np.array(event_1)
+        event_2 = np.array(event_2)
+        
+        if df == True:
+            data = {
+                    "pt_1": pt_1,
+                    "pt_2": pt_2,
+                    "eta_1": eta_1,
+                    "eta_2": eta_2,
+                    "phi_1": phi_1,
+                    "phi_2": phi_2,
+                    "charge_1": cha_1,
+                    "charge_2": cha_2
+                    }
+                
+            data = pd.DataFrame(data) 
+
+    elif p1 == p2 and (c1 == False):
+        events = len(pt1)
+        
+        pt_1 = []
+        eta_1 = []
+        phi_1 = []
+        
+        pt_2 = []
+        eta_2 = []
+        phi_2 = []
+
+        cha_12 = []
+        
+        event_1 = []
+        event_2 = []
+            
+        for i in range(events):
+            if len(pt1[i]) > 1: # At least two particles
+                index1 = np.argmax(pt1[i])
+                trial_pt1 = pt1[i][index1]
+
+                pt1[i][index1] = 1e-1000
+
+                index2 = np.argmax(pt1[i])
+                trial_pt2 = pt1[i][index2]
+
+                if charge1[i][index1] + charge1[i][index2] == charge:
+                        
+                    pt_1.append(trial_pt1)
+                    eta_1.append(eta1[i][index1])
+                    phi_1.append(phi1[i][index1])
+                            
+                    pt_2.append(trial_pt2)
+                    eta_2.append(eta2[i][index2])
+                    phi_2.append(phi2[i][index2])
+
+                    cha_12.append(0)
+                            
+                    event_1.append(i)
+                    event_2.append(i)
+
+        pt_1 = np.array(pt_1)
+        eta_1 = np.array(eta_1)
+        phi_1 = np.array(phi_1)
+            
+        pt_2 = np.array(pt_2)
+        eta_2 = np.array(eta_2)
+        phi_2 = np.array(phi_2)
+
+        cha_12 = np.array(cha_12)
+             
+        event_1 = np.array(event_1)
+        event_2 = np.array(event_2)
+        
+        if df == True:
+            data = {
+                    "pt_1": pt_1,
+                    "pt_2": pt_2,
+                    "eta_1": eta_1,
+                    "eta_2": eta_2,
+                    "phi_1": phi_1,
+                    "phi_2": phi_2,
+                    "charge_12": cha_12,
+                    }
+                
+            data = pd.DataFrame(data) 
+
+
+
+
                     
         
         
